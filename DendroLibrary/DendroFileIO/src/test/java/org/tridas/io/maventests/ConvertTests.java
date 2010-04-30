d7b82bc2a39ae0417be6c56d12fbe82a051a6553
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-04-30 17:52:36 +0000

    Moved test to proper Maven Unit tests

--- DendroLibrary/DendroFileIO/src/test/java/org/tridas/io/maventests/ConvertTests.java
+++ DendroLibrary/DendroFileIO/src/test/java/org/tridas/io/maventests/ConvertTests.java
@@ -1,13 +1,17 @@
-package org.tridas.io.test;
+package org.tridas.io.maventests;
 
 import java.io.IOException;
 
 import org.tridas.io.DendroFile;
 import org.tridas.io.defaults.TridasMetadataFieldSet;
 import org.tridas.io.formats.tridas.TridasReader;
+import org.tridas.io.formats.tridas.TridasWriter;
 import org.tridas.io.formats.tucson.TridasToTucsonDefaults;
 import org.tridas.io.formats.tucson.TucsonFile;
+import org.tridas.io.formats.tucson.TucsonReader;
+import org.tridas.io.formats.tucson.TucsonToTridasDefaults;
 import org.tridas.io.formats.tucson.TucsonWriter;
+import org.tridas.io.naming.UUIDNamingConvention;
 import org.tridas.io.warnings.ConversionWarning;
 import org.tridas.io.warnings.ConversionWarningException;
 import org.tridas.io.warnings.IncompleteTridasDataException;
@@ -15,12 +19,11 @@ import org.tridas.io.warnings.IncorrectDefaultFieldsException;
 import org.tridas.io.warnings.InvalidDendroFileException;
 import org.tridas.schema.TridasProject;
 
+import junit.framework.TestCase;
 
+public class ConvertTests extends TestCase {
 
-public class TestConvertTridasToTucson {
-
-	
-	public static void main(String args[]) 
+	public void testTridasToTucson() 
 	{
 		// Create a dummy project to export.  This would need to
 		// be complete with TridasObject, TridasElement etc for
@@ -36,7 +39,7 @@ public class TestConvertTridasToTucson {
 	    
 		TridasReader reader = new TridasReader();
 		try {
-			reader.loadFile("TestData/TRiDaS", "TridasInvalid.xml", new TridasMetadataFieldSet());
+			reader.loadFile("TestData/TRiDaS", "Tridas1.xml", new TridasMetadataFieldSet());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getLocalizedMessage());
@@ -69,7 +72,7 @@ public class TestConvertTridasToTucson {
 
 
 		// Actually save file(s) to disk
-		tucsonwriter.saveAllToDisk("testTusconOutput");
+		tucsonwriter.saveAllToDisk("TestData/Output");
 
 		
 		// Display any conversion warnings
@@ -80,14 +83,7 @@ public class TestConvertTridasToTucson {
 			for(ConversionWarning cw : tucsonwriter.getWarnings())
 				System.out.println("  - [" + cw.getWarningType().toString()+ "]: " + cw.getMessage());
 		}
-		
-
-
-		
-		// Any warnings can be shown to user if applicable
-		// e.g. if a field was truncated 
-		// tc.getWarnings();
-		
+	
 		
 		// Grab a list of dendro files that will be written
 		DendroFile[] files = tucsonwriter.getFiles();
@@ -97,20 +93,73 @@ public class TestConvertTridasToTucson {
 			TucsonFile f2 = (TucsonFile) f;
 			System.out.println("File output: " + tucsonwriter.getNamingConvention().getFilename(f2));
 		}
-		
-		// Alternatively we could print a string representation of each file
-		/*for (DendroFile f : files)
-		{
-			TucsonFile thisFile = (TucsonFile) f;
-			System.out.println(thisFile.saveToString());
-		}*/
-		
-		
-		
 	}
-	public TestConvertTridasToTucson() 
+	
+	
+	public void testTucsonToTridas() 
 	{
+		// Create a new converter
+		TucsonReader reader = new TucsonReader();
 
+		// Parse the legacy data file
+		try {
+			//TridasEntitiesFromDefaults def = new TridasEntitiesFromDefaults();
+			reader.loadFile("TestData/Tucson", "Tucson1.crn", new TucsonToTridasDefaults());
+		} catch (IOException e) {
+			// Standard IO Exception
+			System.out.println(e.getLocalizedMessage());
+			return;
+		} catch (IncorrectDefaultFieldsException e) {
+			// The default fields you gave were wrong
+			e.printStackTrace();
+		} catch (InvalidDendroFileException e) {
+			// Fatal error interpreting file
+			System.out.println(e.getLocalizedMessage());
+			return;
+		}
+		
+		// Get the metadata lines that were interpreted
+		if (reader.getRawMetadata().size()>0) 
+		System.out.println("Following metadata lines were interpreted");
+		for (String md : reader.getRawMetadata())
+		{
+			System.out.println(md);
+		}
+			
+		// Get any warnings thrown during conversion
+		if(reader.getWarnings().size()>0)
+		System.out.println("Some issues were found whilst converting your file...");
+		for (ConversionWarning w : reader.getWarnings())
+		{
+			System.out.println("  ["+w.getWarningType()+"] -  "+w.getMessage());
+		}		
+		
+		
+		// Extract the TridasProject
+		TridasProject myproject = reader.getProject();
+		
+			
+		TridasWriter writer = new TridasWriter();
+		writer.setNamingConvention(new UUIDNamingConvention());
+		
+		try {
+			writer.loadProject(myproject, new TridasMetadataFieldSet());
+		} catch (IncompleteTridasDataException e) {
+			fail();
+		} catch (ConversionWarningException e) {
+		} catch (IncorrectDefaultFieldsException e) {
+			fail();
+		}
+		writer.saveAllToDisk("TestData/Output");
+		
+		for(DendroFile file : writer.getFiles())
+		{
+			System.out.println("Saved: " + writer.getNamingConvention().getFilename(file)+"."+file.getExtension());
+		}
+		
+		
+		
 	}
+	
 	
 }
