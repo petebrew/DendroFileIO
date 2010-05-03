58281d2a0636f9f3597e4a7f9560e6c17348b40a
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-05-03 20:55:46 +0000

    Added Belfast Archive Format support

---
+++ DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/belfastarchive/BelfastArchiveToTridasDefaults.java
@@ -0,0 +1,9 @@
+package org.tridas.io.formats.belfastarchive;
+
+import org.tridas.io.defaults.IMetadataFieldSet;
+import org.tridas.io.defaults.TridasMetadataFieldSet;
+
+public class BelfastArchiveToTridasDefaults extends TridasMetadataFieldSet
+		implements IMetadataFieldSet {
+
+}
