837af4175eb484b9dcda040c0e3292d70ca191c7
Peter Brewer <p.brewer@ltrr.arizona.edu>
2010-06-10 21:04:37 +0000

    Started a complete refactor of Tucson

---
+++ src/main/java/org/tridas/io/formats/tucson/TucsonToTridasDefaults.java
@@ -0,0 +1,21 @@
+package org.tridas.io.formats.tucson;
+
+import org.tridas.io.defaults.IMetadataFieldSet;
+import org.tridas.io.defaults.TridasMetadataFieldSet;
+import org.tridas.schema.TridasObject;
+
+/**
+ * here for the library user to create and pass in the loadFile() arguments
+ * 
+ * @author Daniel
+ */
+public class TucsonToTridasDefaults extends TridasMetadataFieldSet implements IMetadataFieldSet {
+	
+	// example of customizing
+	@Override
+	protected TridasObject getDefaultTridasObject() {
+		TridasObject object = super.getDefaultTridasObject();
+		object.setComments("Converted from Tucson file");
+		return object;
+	}
+}
