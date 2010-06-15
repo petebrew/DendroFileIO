375f03c8d1114da880274637eadf1a34c125b28b
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-06-15 20:41:51 +0000

    WinDENDRO reader

--- DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/windendro/WinDendroReader.java
+++ DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/windendro/WinDendroReader.java
@@ -2,14 +2,32 @@ package org.tridas.io.formats.windendro;
 
 import java.util.ArrayList;
 
+import org.grlea.log.SimpleLogger;
 import org.tridas.io.AbstractDendroFileReader;
 import org.tridas.io.I18n;
 import org.tridas.io.defaults.IMetadataFieldSet;
+import org.tridas.io.defaults.values.GenericDefaultValue;
+import org.tridas.io.formats.windendro.WinDendroToTridasDefaults.WDDefaultField;
+import org.tridas.io.formats.windendro.WinDendroToTridasDefaults.WinDendroDataType;
+import org.tridas.io.util.DateUtils;
+import org.tridas.io.warningsandexceptions.ConversionWarning;
 import org.tridas.io.warningsandexceptions.InvalidDendroFileException;
+import org.tridas.io.warningsandexceptions.ConversionWarning.WarningType;
+import org.tridas.schema.TridasElement;
+import org.tridas.schema.TridasMeasurementSeries;
+import org.tridas.schema.TridasObject;
 import org.tridas.schema.TridasProject;
+import org.tridas.schema.TridasRadius;
+import org.tridas.schema.TridasSample;
+import org.tridas.schema.TridasValue;
+import org.tridas.schema.TridasValues;
 
 public class WinDendroReader extends AbstractDendroFileReader {
-
+	
+	private static final SimpleLogger log = new SimpleLogger(WinDendroReader.class);	
+	private ArrayList<WinDendroSeries> seriesList = new ArrayList<WinDendroSeries>();
+	
+	private WinDendroToTridasDefaults defaults = null;      // defaults given by user
 	Integer currentLineNumber = -1;
 	
 	public WinDendroReader() {
@@ -23,8 +41,7 @@ public class WinDendroReader extends AbstractDendroFileReader {
 
 	@Override
 	public IMetadataFieldSet getDefaults() {
-		// TODO Auto-generated method stub
-		return null;
+		return defaults;
 	}
 
 	@Override
@@ -34,8 +51,7 @@ public class WinDendroReader extends AbstractDendroFileReader {
 
 	@Override
 	public String[] getFileExtensions() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[]{"txt"};
 	}
 
 	@Override
@@ -45,8 +61,54 @@ public class WinDendroReader extends AbstractDendroFileReader {
 
 	@Override
 	public TridasProject getProject() {
-		// TODO Auto-generated method stub
-		return null;
+		TridasProject project = defaults.getProjectWithDefaults(false);
+		ArrayList<TridasObject> olist = new ArrayList<TridasObject>();
+		
+		for (WinDendroSeries series : seriesList)
+		{
+			TridasObject o = series.defaults.getObjectWithDefaults();
+			TridasElement e = series.defaults.getElementWithDefaults();
+			TridasSample s = series.defaults.getSampleWithDefaults();
+			
+			// Measurement Series
+			
+			TridasRadius r = series.defaults.getRadiusWithDefaults(false);
+			TridasMeasurementSeries ms = series.defaults.getMeasurementSeriesWithDefaults();
+			ArrayList<TridasValues> valuesGroupList = new ArrayList<TridasValues>();
+			TridasValues values = series.defaults.getDefaultTridasValues();
+			ArrayList<TridasValue> valuesList = new ArrayList<TridasValue>();
+			
+			for (Double val : series.dataDoubles)
+			{
+				TridasValue value = new TridasValue();
+				value.setValue(String.valueOf(val));
+				valuesList.add(value);
+			}
+			
+			values.setValues(valuesList);
+			valuesGroupList.add(values);
+			ms.setValues(valuesGroupList);
+			
+
+			
+			ArrayList<TridasMeasurementSeries> mslist = new ArrayList<TridasMeasurementSeries>();
+			mslist.add(ms);
+			r.setMeasurementSeries(mslist);
+			ArrayList<TridasRadius> rlist = new ArrayList<TridasRadius>();
+			rlist.add(r);
+			s.setRadiuses(rlist);
+			ArrayList<TridasSample> slist = new ArrayList<TridasSample>();
+			slist.add(s);
+			e.setSamples(slist);
+			ArrayList<TridasElement> elist = new ArrayList<TridasElement>();
+			elist.add(e);
+			o.setElements(elist);
+			olist.add(o);
+						
+		}
+		
+		project.setObjects(olist);
+		return project;
 	}
 
 	@Override
@@ -55,18 +117,192 @@ public class WinDendroReader extends AbstractDendroFileReader {
 	}
 
 	@Override
-	protected void parseFile(String[] argFileString,
-			IMetadataFieldSet argDefaultFields)
-			throws InvalidDendroFileException {
-		
-		checkFileIsValid(argFileString);
-
-	}
-
-	@Override
 	protected void resetReader() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void parseFile(String[] argFileString,
+			IMetadataFieldSet argDefaultFields)
+			throws InvalidDendroFileException {
+		log.debug("Parsing: " + argFileString);
+		defaults = (WinDendroToTridasDefaults) argDefaultFields;
+		
+		checkFileIsValid(argFileString);
+
+		// Loop through all the data lines
+		for (int i=2; i<argFileString.length; i++)
+		{
+			// Skip blank or tab filled lines
+			if (argFileString[i].replaceAll("\\t", "").length()<50) continue;
+			
+			String[] data = argFileString[i].split("\\t");
+			WinDendroToTridasDefaults seriesdefaults = (WinDendroToTridasDefaults) defaults.clone();
+			WinDendroSeries currentSeries = new WinDendroSeries(seriesdefaults);
+			this.currentLineNumber = i+1; 
+
+			/**
+			 * SET ALL VERSION 3 FIELDS
+			 */
+			// Tree name
+			currentSeries.defaults.getStringDefaultValue(WDDefaultField.TREE_NAME).setValue(data[0]);
+			
+			// Path ID		
+			if(!data[1].trim().equals(""))
+			{
+				currentSeries.defaults.getStringDefaultValue(WDDefaultField.PATH_ID).setValue(data[1]);
+			}
+
+			// Site ID
+			currentSeries.defaults.getStringDefaultValue(WDDefaultField.SITE_ID).setValue(data[2]);
+
+			// Last Ring year
+			try{
+				currentSeries.defaults.getSafeIntYearDefaultValue(WDDefaultField.LAST_RING_YEAR).setValue(Integer.parseInt(data[3]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidLastRing", currentLineNumber+"")));
+			}
+			
+			// Sapwood Distance
+			try{
+				currentSeries.defaults.getDoubleDefaultValue(WDDefaultField.SAPWOOD_DISTANCE).setValue(Double.parseDouble(data[4]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidSapwoodDistance", currentLineNumber+"")));
+			}
+			
+			// Tree height
+			try{
+				currentSeries.defaults.getDoubleDefaultValue(WDDefaultField.TREE_HEIGHT).setValue(Double.parseDouble(data[5]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidTreeHeight", currentLineNumber+"")));
+			}
+			
+			// Tree age
+			try{
+				currentSeries.defaults.getIntegerDefaultValue(WDDefaultField.TREE_AGE).setValue(Integer.parseInt(data[6]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidTreeAge", currentLineNumber+"")));
+			}
+			
+			// Section height
+			try{
+				currentSeries.defaults.getDoubleDefaultValue(WDDefaultField.SECTION_HEIGHT).setValue(Double.parseDouble(data[7]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidSectionHeight", currentLineNumber+"")));
+			}
+			
+			// User variable
+			currentSeries.defaults.getStringDefaultValue(WDDefaultField.USER_VARIABLE).setValue(data[8]);
+			
+			// Ring count
+			try{
+				currentSeries.defaults.getIntegerDefaultValue(WDDefaultField.RING_COUNT).setValue(Integer.parseInt(data[9]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidRingCount", currentLineNumber+"")));
+			}
+			
+			// Data type
+			GenericDefaultValue<WinDendroDataType> dataTypeField = (GenericDefaultValue<WinDendroDataType>) currentSeries.defaults
+				.getDefaultValue(WDDefaultField.WD_DATA_TYPE);
+			if(WinDendroDataType.fromCode(data[10])!=null)
+			{
+				dataTypeField.setValue(WinDendroDataType.fromCode(data[10]));
+			}
+			else
+			{
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidDataType", currentLineNumber+"")));
+			}
+			
+			// Offset to next 
+			try{
+				currentSeries.defaults.getIntegerDefaultValue(WDDefaultField.OFFSET_TO_NEXT).setValue(Integer.parseInt(data[11]));
+			} catch (NumberFormatException e){
+				addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidOffsetToNext", currentLineNumber+"")));
+			}
+						
+			/**
+			 * SET VERSION 4 FIELDS
+			 */
+			if(getFormatVersion(argFileString)>=4)
+			{
+				// Average disk diameter
+				try{
+					currentSeries.defaults.getDoubleDefaultValue(WDDefaultField.DISK_AVG_DIAM).setValue(Double.parseDouble(data[30]));
+				} catch (NumberFormatException e){
+					addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidDiskAvDiam", currentLineNumber+"")));
+				}
+			
+				// Path Length
+				try{
+					currentSeries.defaults.getDoubleDefaultValue(WDDefaultField.PATH_LENGTH).setValue(Double.parseDouble(data[34]));
+				} catch (NumberFormatException e){
+					addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidPathLength", currentLineNumber+"")));
+				}
+				
+				// Analysis timestamp
+				if(!data[13].trim().equals(""))
+				{
+					if(DateUtils.getDateTimeFromWinDendroTimestamp(data[13])!=null)
+					{
+						currentSeries.defaults.getDateTimeDefaultValue(WDDefaultField.ANALYSIS_TIMESTAMP).
+							setValue(DateUtils.getDateTimeFromWinDendroTimestamp(data[13]));
+					}
+					else
+					{
+						addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("windendro.invalidAnalysisTimestamp", currentLineNumber+"")));
+					}
+				}
+				
+			}
+			
+			
+			/**
+			 * EXTRACT ACTUAL DATA
+			 */
+			for (int col=getColWhereDataStarts(argFileString)-1; col < data.length; col++)
+			{
+				if(data[col].equals(""))
+				{
+					break;
+				}
+				
+				try{
+				currentSeries.dataDoubles.add(Double.parseDouble(data[col]));
+				} catch (NumberFormatException e)
+				{
+					addWarning(new ConversionWarning(WarningType.INVALID, I18n.getText("fileio.invalidDataValue") + ": "+ data[col] ));
+				}
+			}
+			if(currentSeries.dataDoubles.size()>0)
+			{
+				seriesList.add(currentSeries);
+			}
+		}
+		
+		
+	}
+	
+	/**
+	 * Get the column where the data values start
+	 * 
+	 * @param argFileString
+	 * @return
+	 */
+	private Integer getColWhereDataStarts(String[] argFileString)
+	{
+		String[] parts = argFileString[0].split("\\t");
+		if (parts.length!=8)
+		{
+			return null;
+		}
+		
+		try{
+		return Integer.parseInt(parts[3]);
+		} catch (NumberFormatException e){}
+		
+		return null;
 	}
 	
 	
@@ -126,5 +362,42 @@ public class WinDendroReader extends AbstractDendroFileReader {
 		}
 				
 	}
+	
+	/**
+	 * Extract the version number from the header
+	 * 
+	 * @param argFileString
+	 * @return
+	 */
+	private Integer getFormatVersion(String[] argFileString)
+	{
+		// Catch problems with the header line 1
+		String[] headerParts = argFileString[0].split("\\t");
+		
+		if (headerParts.length!=8) return null;
+		
+		try{ 
+			return Integer.parseInt(headerParts[1]);
+		} catch (NumberFormatException e){}
+		
+		return null;
 
+	}
+	
+	
+	/**
+	 * Class to store the measurement series data
+	 * 
+	 * @author peterbrewer
+	 */
+	private static class WinDendroSeries{
+		public WinDendroToTridasDefaults defaults;
+		public final ArrayList<Double> dataDoubles = new ArrayList<Double>();
+		
+		private WinDendroSeries(WinDendroToTridasDefaults df)
+		{
+			defaults =df;
+		}
+	}
+	
 }
--- DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/windendro/WinDendroToTridasDefaults.java
+++ DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/windendro/WinDendroToTridasDefaults.java
@@ -1,9 +1,318 @@
 package org.tridas.io.formats.windendro;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.tridas.io.I18n;
 import org.tridas.io.defaults.IMetadataFieldSet;
 import org.tridas.io.defaults.TridasMetadataFieldSet;
+import org.tridas.io.defaults.values.DateTimeDefaultValue;
+import org.tridas.io.defaults.values.DoubleDefaultValue;
+import org.tridas.io.defaults.values.GenericDefaultValue;
+import org.tridas.io.defaults.values.IntegerDefaultValue;
+import org.tridas.io.defaults.values.SafeIntYearDefaultValue;
+import org.tridas.io.defaults.values.StringDefaultValue;
+import org.tridas.io.util.DateUtils;
+import org.tridas.io.util.SafeIntYear;
+import org.tridas.schema.DatingSuffix;
+import org.tridas.schema.NormalTridasUnit;
+import org.tridas.schema.NormalTridasVariable;
+import org.tridas.schema.TridasDimensions;
+import org.tridas.schema.TridasElement;
+import org.tridas.schema.TridasGenericField;
+import org.tridas.schema.TridasIdentifier;
+import org.tridas.schema.TridasMeasurementSeries;
+import org.tridas.schema.TridasMeasuringMethod;
+import org.tridas.schema.TridasObject;
+import org.tridas.schema.TridasSample;
+import org.tridas.schema.TridasUnit;
+import org.tridas.schema.TridasValues;
+import org.tridas.schema.TridasVariable;
 
 public class WinDendroToTridasDefaults extends TridasMetadataFieldSet implements
 		IMetadataFieldSet {
 
+	public enum WDDefaultField {
+		TREE_NAME, PATH_ID, SITE_ID, LAST_RING_YEAR, SAPWOOD_DISTANCE, TREE_HEIGHT, TREE_AGE, SECTION_HEIGHT,
+		USER_VARIABLE, RING_COUNT, WD_DATA_TYPE, OFFSET_TO_NEXT, IMAGE_NAME, ANALYSIS_TIMESTAMP, ACQUISITION_TIMESTAMP,
+		MODIFIED_DATE, IMAGE_SIZE_PIXELS, CALIB_METHOD, IMAGEING_HARDWARE, LENS_FOC_LENGTH, DISK_AVG_DIAM, PATH_LENGTH;
+	}
+	
+	/**
+	 * @see org.tridas.io.defaults.AbstractMetadataFieldSet#initDefaultValues()
+	 */
+	@Override
+	protected void initDefaultValues() {
+		super.initDefaultValues();
+		
+		// Version 3 fields
+		setDefaultValue(WDDefaultField.TREE_NAME, new StringDefaultValue(UUID.randomUUID().toString()));
+		setDefaultValue(WDDefaultField.PATH_ID, new StringDefaultValue(UUID.randomUUID().toString()));
+		setDefaultValue(WDDefaultField.SITE_ID, new StringDefaultValue(UUID.randomUUID().toString()));
+		setDefaultValue(WDDefaultField.LAST_RING_YEAR, new SafeIntYearDefaultValue());
+		setDefaultValue(WDDefaultField.SAPWOOD_DISTANCE, new DoubleDefaultValue());
+		setDefaultValue(WDDefaultField.TREE_HEIGHT, new DoubleDefaultValue(null, 1.0, 150.0));  // Tallest tree is ~115m
+		setDefaultValue(WDDefaultField.TREE_AGE, new IntegerDefaultValue());
+		setDefaultValue(WDDefaultField.SECTION_HEIGHT, new DoubleDefaultValue(null, 0.0, 150.0));
+		setDefaultValue(WDDefaultField.USER_VARIABLE, new StringDefaultValue());
+		setDefaultValue(WDDefaultField.RING_COUNT, new IntegerDefaultValue(null, 1, Integer.MAX_VALUE));
+		setDefaultValue(WDDefaultField.WD_DATA_TYPE, new GenericDefaultValue<WinDendroDataType>());
+		setDefaultValue(WDDefaultField.OFFSET_TO_NEXT, new IntegerDefaultValue(null, 0, 99));
+		
+		
+		// Version 4 fields
+		// TODO
+		setDefaultValue(WDDefaultField.ANALYSIS_TIMESTAMP, new DateTimeDefaultValue(DateUtils.getTodaysDateTime()));
+		setDefaultValue(WDDefaultField.DISK_AVG_DIAM, new DoubleDefaultValue());
+		setDefaultValue(WDDefaultField.PATH_LENGTH, new DoubleDefaultValue());
+
+	}
+	
+	@Override
+	public TridasObject getDefaultTridasObject()
+	{
+		TridasObject o = super.getDefaultTridasObject();
+		
+		TridasIdentifier id = new TridasIdentifier();
+		id.setDomain(getStringDefaultValue(TridasMandatoryField.IDENTIFIER_DOMAIN).getValue());
+		id.setValue(getStringDefaultValue(WDDefaultField.SITE_ID).getValue());
+		o.setIdentifier(id);
+		o.setTitle(getStringDefaultValue(WDDefaultField.SITE_ID).getValue());
+		
+		return o;
+		
+	}
+	
+	@Override
+	public TridasElement getDefaultTridasElement()
+	{
+		TridasElement e = super.getDefaultTridasElement();
+		
+		TridasIdentifier id = new TridasIdentifier();
+		id.setDomain(getStringDefaultValue(TridasMandatoryField.IDENTIFIER_DOMAIN).getValue());
+		id.setValue(getStringDefaultValue(WDDefaultField.TREE_NAME).getValue());
+		e.setIdentifier(id);
+		e.setTitle(getStringDefaultValue(WDDefaultField.TREE_NAME).getValue());
+		
+		// Set dimensions
+		if (getDoubleDefaultValue(WDDefaultField.DISK_AVG_DIAM).getValue()!=null && 
+			getDoubleDefaultValue(WDDefaultField.TREE_HEIGHT).getValue()!=null)
+		{
+			TridasDimensions dim = new TridasDimensions();
+			TridasUnit unit = new TridasUnit();
+			unit.setNormalTridas(NormalTridasUnit.METRES);
+			dim.setUnit(unit);
+			
+			dim.setDiameter(BigDecimal.valueOf(getDoubleDefaultValue(WDDefaultField.DISK_AVG_DIAM).getValue()));
+			dim.setHeight(BigDecimal.valueOf(getDoubleDefaultValue(WDDefaultField.TREE_HEIGHT).getValue()));
+			e.setDimensions(dim);
+		}
+		
+		return e;
+		
+	}
+	
+	
+	@Override
+	public TridasSample getDefaultTridasSample()
+	{
+		TridasSample s = super.getDefaultTridasSample();
+		
+		//
+		if(getDoubleDefaultValue(WDDefaultField.SECTION_HEIGHT).getValue()!=null && 
+		  !getDoubleDefaultValue(WDDefaultField.SECTION_HEIGHT).getValue().equals(0.0)	)
+		{
+			s.setPosition(String.valueOf(getDoubleDefaultValue(WDDefaultField.SECTION_HEIGHT).getValue())+" "+I18n.getText("windendro.metresFromGround"));
+		}
+		
+		return s;
+		
+	}
+	
+	@Override
+	public TridasMeasurementSeries getDefaultTridasMeasurementSeries()
+	{
+		TridasMeasurementSeries ms = super.getDefaultTridasMeasurementSeries();
+		TridasIdentifier id = new TridasIdentifier();
+		id.setDomain(getStringDefaultValue(TridasMandatoryField.IDENTIFIER_DOMAIN).getValue());
+		id.setValue(getStringDefaultValue(WDDefaultField.PATH_ID).getValue());
+		ms.setIdentifier(id);
+		ms.setTitle(getStringDefaultValue(WDDefaultField.PATH_ID).getValue());
+		
+		// Analysis timestamp
+		ms.setCreatedTimestamp(getDateTimeDefaultValue(WDDefaultField.ANALYSIS_TIMESTAMP).getValue());
+		
+		// Set measuring method 
+		TridasMeasuringMethod method = new TridasMeasuringMethod();
+		method.setNormal("Image analysis using WinDENDRO");
+		ms.setMeasuringMethod(method);
+		
+		SafeIntYear lastYear = getSafeIntYearDefaultValue(WDDefaultField.LAST_RING_YEAR).getValue();
+		if (lastYear!=null)
+		{
+			// Set year of last ring
+			ms.getInterpretation().setLastYear(lastYear.toTridasYear(DatingSuffix.AD));
+
+			if(getIntegerDefaultValue(WDDefaultField.RING_COUNT).getValue()!=null)
+			{
+				// Set first year by using ring count and last ring year
+				SafeIntYear firstYear =lastYear.add(0-(getIntegerDefaultValue(WDDefaultField.RING_COUNT).getValue()));
+				ms.getInterpretation().setFirstYear(firstYear.toTridasYear(DatingSuffix.AD));
+				
+				if(getIntegerDefaultValue(WDDefaultField.TREE_AGE).getValue()==0)
+				{
+					// If tree age = 0 then spec says to use ring count to decide pith year
+					SafeIntYear pithAge =lastYear.add(0-(getIntegerDefaultValue(WDDefaultField.RING_COUNT).getValue()));
+					ms.getInterpretation().setPithYear(pithAge.toTridasYear(DatingSuffix.AD));
+				}
+				else
+				{
+					// tree age has been set, so use it to calculate pith year
+					SafeIntYear pithAge =lastYear.add(0-(getIntegerDefaultValue(WDDefaultField.TREE_AGE).getValue()));
+					ms.getInterpretation().setPithYear(pithAge.toTridasYear(DatingSuffix.AD));
+				}
+				
+			}
+			
+
+			
+
+		}
+	
+		ArrayList<TridasGenericField> gflist = new ArrayList<TridasGenericField>();
+		
+		if(getDoubleDefaultValue(WDDefaultField.SAPWOOD_DISTANCE).getValue()!=null &&
+		   !getDoubleDefaultValue(WDDefaultField.SAPWOOD_DISTANCE).getValue().equals(0.0))
+		{
+			TridasGenericField gf = new TridasGenericField();
+			gf.setName("windendro.sapwoodDistance");
+			gf.setType("xs:float");
+			gf.setValue(String.valueOf(getDoubleDefaultValue(WDDefaultField.SAPWOOD_DISTANCE).getValue()));
+			gflist.add(gf);
+		}
+		if(getStringDefaultValue(WDDefaultField.USER_VARIABLE).getValue()!=null && 
+		   !getStringDefaultValue(WDDefaultField.USER_VARIABLE).getValue().equals("0"))
+				{
+					TridasGenericField gf = new TridasGenericField();
+					gf.setName("windendro.userVariable");
+					gf.setType("xs:string");
+					gf.setValue(getStringDefaultValue(WDDefaultField.USER_VARIABLE).getValue());
+					gflist.add(gf);
+				}
+		
+		
+		ms.setGenericFields(gflist);		
+				
+		
+		return ms;
+		
+	}
+	
+	
+	protected TridasValues getDefaultTridasValues()
+	{
+		TridasValues values = new TridasValues();
+		TridasVariable variable = new TridasVariable();
+		TridasUnit unit = new TridasUnit();
+		
+		
+		
+		// Variable and Units
+		
+		if(getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue()==null)
+		{
+			variable.setValue("Unknown");
+			unit.setValue("Unknown");
+		}
+		else if(getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.RINGWIDTH))
+		{
+			variable.setNormalTridas((NormalTridasVariable.RING_WIDTH));
+			unit.setNormalTridas(NormalTridasUnit.MILLIMETRES);
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.EARLYWIDTH))
+		{
+			variable.setNormalTridas((NormalTridasVariable.EARLYWOOD_WIDTH));
+			unit.setNormalTridas(NormalTridasUnit.MILLIMETRES);
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.LATEWIDTH))
+		{
+			variable.setNormalTridas((NormalTridasVariable.LATEWOOD_WIDTH));
+			unit.setNormalTridas(NormalTridasUnit.MILLIMETRES);
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.DENSITY))
+		{
+			variable.setNormalTridas((NormalTridasVariable.RING_DENSITY));
+			unit.setValue("g/cm3");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.EARLYDENSITY))
+		{
+			variable.setNormalTridas((NormalTridasVariable.EARLYWOOD_DENSITY));
+			unit.setValue("g/cm3");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.EARLYWIDTHPERC))
+		{
+			variable.setValue("Earlywood width in percentage of ring width");
+			unit.setValue("Percent");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.LATEDENSITY))
+		{
+			variable.setNormalTridas((NormalTridasVariable.LATEWOOD_DENSITY));
+			unit.setValue("g/cm3");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.LATEWIDTHPERC))
+		{
+			variable.setValue("Latewood width in percentage of ring width");
+			unit.setValue("Percent");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.MAXDENSITY))
+		{
+			variable.setNormalTridas((NormalTridasVariable.MAXIMUM_DENSITY));
+			unit.setValue("g/cm3");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.MINDENSITY))
+		{
+			variable.setValue("Minimum density");
+			unit.setValue("g/cm3");
+		}
+		else if (getDefaultValue(WDDefaultField.WD_DATA_TYPE).getValue().equals(WinDendroDataType.RINGANGLE))
+		{
+			variable.setValue("Ring angle");
+			unit.setValue("Radians");
+		}
+		
+		values.setVariable(variable);
+		values.setUnit(unit);
+		return values;
+	}
+	
+	
+	
+	
+	public enum WinDendroDataType {
+		RINGWIDTH("RINGWIDTH"), EARLYWIDTH("EARLYWIDTH"), LATEWIDTH("LATEWIDTH"), EARLYWIDTHPERC("EARLYWIDTH%"),
+		LATEWIDTHPERC("LATEWIDTH%"), DENSITY("DENSITY"), EARLYDENSITY("EARLYDENSITY"), LATEDENSITY("LATEDENSITY"),
+		MAXDENSITY("MAXDENSITY"), MINDENSITY("MINDENSITY"), RINGANGLE("RINGANGLE");
+		
+		private String code;
+		
+		WinDendroDataType(String c) {
+			code = c;
+		}
+		
+		@Override
+		public final String toString() {
+			return code;
+		}
+		
+		public static WinDendroDataType fromCode(String code) {
+			for (WinDendroDataType val : WinDendroDataType.values()) {
+				if (val.toString().equalsIgnoreCase(code)) {
+					return val;
+				}
+			}
+			return null;
+		}
+	}
+	
 }
