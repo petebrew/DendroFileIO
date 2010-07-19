721ab31b7794809645c1fb9c6b7e755089124fdb
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-07-19 14:21:10 +0000

    Fixing bugs notified by Susannah Tysor

--- DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/excelmatrix/ExcelMatrixFile.java
+++ DendroLibrary/DendroFileIO/src/main/java/org/tridas/io/formats/excelmatrix/ExcelMatrixFile.java
@@ -285,7 +285,7 @@ public class ExcelMatrixFile implements IDendroFile {
 			thisStartYear = new SafeIntYear(series.getInterpretation().getFirstYear());
 		} catch (NullPointerException e) {}
 		
-		Integer row = 1;
+		Integer row = 2;
 		
 		for (SafeIntYear currYear = yrRange.getStart(); currYear.compareTo(thisStartYear) < 0; currYear = currYear
 				.add(1)) {
