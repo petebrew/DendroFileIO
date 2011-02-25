d38bfd5076a0e8b74090f692dc4f40836c5c1f90
Peter Brewer <p.brewer@ltrr.arizona.edu>
2011-02-25 17:22:44 +0000

    Initial incomplete commit of ODF format

---
+++ src/main/java/org/tridas/io/formats/odfmatrix/ODFMatrixFile.java
@@ -0,0 +1,375 @@
+/**
+ * Copyright 2010 Peter Brewer and Daniel Murphy
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tridas.io.formats.odfmatrix;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.CellView;
+import jxl.Workbook;
+import jxl.format.Colour;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+import org.odftoolkit.odfdom.doc.OdfDocument;
+import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
+import org.odftoolkit.odfdom.doc.table.OdfTable;
+import org.odftoolkit.odfdom.doc.table.OdfTableCell;
+import org.odftoolkit.odfdom.doc.table.OdfTableColumn;
+import org.odftoolkit.odfdom.doc.table.OdfTableRow;
+import org.odftoolkit.odfdom.dom.element.office.OfficeSpreadsheetElement;
+import org.odftoolkit.odfdom.incubator.doc.office.OdfOfficeAutomaticStyles;
+import org.odftoolkit.odfdom.incubator.doc.office.OdfOfficeStyles;
+import org.odftoolkit.odfdom.pkg.OdfFileDom;
+import org.tridas.interfaces.ITridasSeries;
+import org.tridas.io.I18n;
+import org.tridas.io.IDendroFile;
+import org.tridas.io.defaults.IMetadataFieldSet;
+import org.tridas.io.exceptions.ConversionWarningException;
+import org.tridas.io.util.SafeIntYear;
+import org.tridas.io.util.UnitUtils;
+import org.tridas.io.util.YearRange;
+import org.tridas.schema.DatingSuffix;
+import org.tridas.schema.NormalTridasUnit;
+import org.tridas.schema.TridasGenericField;
+import org.tridas.schema.TridasValue;
+import org.tridas.schema.TridasValues;
+
+public class ODFMatrixFile implements IDendroFile {
+	
+	private ArrayList<ITridasSeries> seriesList = new ArrayList<ITridasSeries>();
+	private YearRange yrRange;
+	private DatingSuffix calendar = DatingSuffix.AD;
+	private IMetadataFieldSet defaults;
+	
+	public ODFMatrixFile(IMetadataFieldSet argDefaults) {
+		defaults = argDefaults;
+	}
+	
+	public void setSeriesList(ArrayList<ITridasSeries> lst) {
+		
+		
+		// Switch the BP dating if any series are in BP
+		for (ITridasSeries ser : lst) {
+			
+			if (calendar == DatingSuffix.BP) {
+				break;
+			}
+			try {
+				if (ser.getInterpretation().getFirstYear().getSuffix() == DatingSuffix.BP) {
+					calendar = DatingSuffix.BP;
+				}
+			} catch (NullPointerException e) {}
+		}
+		
+
+		
+		
+		// Calculate the range for these series
+		for (ITridasSeries ser : lst) {
+			Integer ringcount = ser.getValues().get(0).getValues().size();
+			SafeIntYear startYear = null;
+			try {
+				
+				// Make sure we're using years with the right calendar
+				//Year yearsWithCalendar = new SafeIntYear(ser.getInterpretation().getFirstYear()).toTridasYear(calendar);
+				startYear = new SafeIntYear(ser.getInterpretation().getFirstYear());
+				
+			} catch (NullPointerException e) {
+				startYear = new SafeIntYear();
+			}
+			
+			YearRange thisrange = new YearRange(startYear, ringcount);
+			
+			if (yrRange == null) {
+				yrRange = thisrange;
+			}
+			
+			yrRange = yrRange.union(thisrange);
+		}
+		
+		// Set the list
+		seriesList = lst;
+	}
+	
+	@Override
+	public String getExtension() {
+		return "ods";
+	}
+	
+	@Override
+	public ITridasSeries[] getSeries() {
+		return seriesList.toArray(new ITridasSeries[0]);
+
+	}
+	
+	@Override
+	public String[] saveToString() {
+		
+		throw new UnsupportedOperationException(I18n.getText("fileio.binaryAsStringUnsupported"));
+		
+	}
+	
+	/**
+	 * An alternative to the normal saveToString() as this is a binary format
+	 * 
+	 * @param os
+	 * @throws IOException
+	 * @throws WriteException
+	 */
+	public void saveToDisk(OutputStream os) throws IOException, WriteException {
+		String inputFileName; 
+		String outputFileName; 
+		OdfSpreadsheetDocument outputDocument;
+
+		OdfFileDom contentDom; // the document object model for content.xml 
+		OdfFileDom stylesDom; // the document object model for styles.xml 
+		OdfOfficeAutomaticStyles contentAutoStyles;
+		OdfOfficeStyles stylesOfficeStyles;
+		String columnStyleName; 
+		String rowStyleName; 
+		String headingStyleName; 
+		String noaaTimeStyleName; 
+		String noaaDateStyleName; 
+		String noaaTempStyleName;
+		OfficeSpreadsheetElement officeSpreadsheet;
+		
+		
+		try {
+			outputDocument = OdfSpreadsheetDocument.newSpreadsheetDocument();
+			contentDom = outputDocument.getContentDom(); 
+			stylesDom = outputDocument.getStylesDom(); 
+			contentAutoStyles = contentDom.getOrCreateAutomaticStyles(); 
+			stylesOfficeStyles = outputDocument.getOrCreateDocumentStyles(); 
+			officeSpreadsheet = outputDocument.getContentRoot(); 
+			
+			OdfTable table; 
+			
+			table = outputDocument.getTableByName("Sheet1");
+			table.setTableName(I18n.getText("general.data"));
+						
+
+
+			writeYearHeaderCol(table);
+			
+			int col = 1;
+			for (ITridasSeries series : seriesList) {
+				writeRingWidthColumn(table, col, series);
+				// writeMetadataColumn(metadataSheet, col, series);
+				col++;
+			}
+			
+			outputDocument.save(os);
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new IOException("Problem creating spreadsheet");
+		}
+		
+	}
+	
+	/**
+	 * Get the format for the standard header
+	 * 
+	 * @return
+	 */
+	private static WritableCellFormat getHeaderFormat() {
+		// Create the Header Format
+		WritableFont headerFont = new WritableFont(WritableFont.ARIAL, 12, WritableFont.BOLD);
+		WritableCellFormat headerFormat = new WritableCellFormat(headerFont);
+		try {
+			headerFormat.setWrap(false);
+			headerFormat.setBackground(Colour.PALE_BLUE);
+		} catch (WriteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return headerFormat;
+	}
+	
+	/**
+	 * Get the format for the standard data cell
+	 * 
+	 * @return
+	 */
+	private static WritableCellFormat getDataFormat() {
+		// Create the Data Format
+		WritableFont dataFont = new WritableFont(WritableFont.ARIAL, 12, WritableFont.NO_BOLD);
+		WritableCellFormat dataFormat = new WritableCellFormat(dataFont);
+		try {
+			dataFormat.setWrap(false);
+		} catch (WriteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dataFormat;
+	}
+		
+	/**
+	 * Write the range of years in the first column of the worksheet
+	 * 
+	 * @param table
+	 * @throws WriteException
+	 */
+	private void writeYearHeaderCol(OdfTable table) throws WriteException {
+		if (yrRange == null) {
+			return;
+		}
+		
+		// Create year label
+		String yearlabel = I18n.getText("general.years");
+		if (calendar == DatingSuffix.BP) {
+			yearlabel += " (" + I18n.getText("general.years.bp") + ")";
+		}
+		else if ((yrRange.getStart().compareTo(new SafeIntYear("-1")) < 0)
+				&& (yrRange.getEnd().compareTo(new SafeIntYear("1")) > 0)) {
+			yearlabel += " (" + I18n.getText("general.years.bc") + "/" + I18n.getText("general.years.ad") + ")";
+		}
+		else if ((yrRange.getStart().compareTo(new SafeIntYear("-1")) < 0)
+				&& (yrRange.getEnd().compareTo(new SafeIntYear("1")) <= 0)) {
+			yearlabel += " (" + I18n.getText("general.years.bc") + ")";
+		}
+		else if ((yrRange.getStart().compareTo(new SafeIntYear("-1")) >= 0)
+				&& (yrRange.getEnd().compareTo(new SafeIntYear("1")) > 0)) {
+			yearlabel += " (" + I18n.getText("general.years.ad") + ")";
+		}
+	
+		table.getCellByPosition(0, 0).setStringValue(yearlabel);
+		
+		SafeIntYear yr = yrRange.getStart();
+		Integer rowNumber = 1;
+		Double yearval;
+		while (yr.compareTo(yrRange.getEnd()) <= 0) {
+			yearval = Double.parseDouble(yr.toTridasYear(calendar).getValue().toString());
+			
+			table.getCellByPosition(0, rowNumber).setDoubleValue(yearval);
+			
+			// Increment to next year and row number
+			yr = yr.add(1);
+			rowNumber++;
+		}
+		
+	}
+	
+	private void writeMetadataColumn(WritableSheet s, Integer col, ITridasSeries series) throws RowsExceededException,
+			WriteException {
+		// Creates year label
+		Label l;
+		String keycode = null;
+		if(series.isSetGenericFields())
+		{
+			for(TridasGenericField gf : series.getGenericFields())
+			{
+				if(gf.getName().toLowerCase().equals("keycode"))
+				{
+					keycode = gf.getValue();
+				}
+			}
+		}
+		if(keycode!=null)
+		{
+			l = new Label(col, 0, keycode, getHeaderFormat());
+		}
+		else
+		{
+			l = new Label(col, 0, series.getTitle(), getHeaderFormat());
+		}
+		s.addCell(l);
+		
+	}
+	
+	/**
+	 * Write the ring widths for the provided series in the specified column
+	 * 
+	 * @param table
+	 * @param col
+	 * @param series
+	 * @throws RowsExceededException
+	 * @throws WriteException
+	 */
+	private void writeRingWidthColumn(OdfTable table, Integer col, ITridasSeries series) throws RowsExceededException,
+			WriteException {
+		List<TridasValue> values = series.getValues().get(0).getValues();
+		
+		// Creates year label
+		String l;
+		String keycode = null;
+		if(series.isSetGenericFields())
+		{
+			for(TridasGenericField gf : series.getGenericFields())
+			{
+				if(gf.getName().toLowerCase().equals("keycode"))
+				{
+					keycode = gf.getValue();
+				}
+			}
+		}
+		if(keycode!=null)
+		{
+			l = keycode;
+		}
+		else
+		{
+		    l = series.getTitle();
+		}
+		
+		table.getCellByPosition(col, 0).setStringValue(l);
+		
+		// Calculate which row to start on
+		SafeIntYear thisStartYear = new SafeIntYear();
+		try {
+			thisStartYear = new SafeIntYear(series.getInterpretation().getFirstYear());
+		} catch (NullPointerException e) {}
+		
+		Integer row = 1;
+		
+		for (SafeIntYear currYear = yrRange.getStart(); currYear.compareTo(thisStartYear) < 0; currYear = currYear
+				.add(1)) {
+			table.getCellByPosition(col, row).setStringValue("");
+			row++;
+		}
+		
+		// Loop through values and write to spreadsheet
+		Double yearval;
+		for (TridasValue value : values) {
+			if(value.getValue()==null);
+			
+			yearval = Double.parseDouble(value.getValue());
+			table.getCellByPosition(col, row).setDoubleValue(yearval);
+			row++;
+		}
+		
+	}
+	
+	/**
+	 * @see org.tridas.io.IDendroFile#getDefaults()
+	 */
+	@Override
+	public IMetadataFieldSet getDefaults() {
+		return defaults;
+	}
+}
---
+++ src/main/java/org/tridas/io/formats/odfmatrix/ODFMatrixReader.java
@@ -0,0 +1,433 @@
+/*******************************************************************************
+ * Copyright 2011 Peter Brewer and Daniel Murphy
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.tridas.io.formats.odfmatrix;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jxl.Cell;
+import jxl.CellType;
+import jxl.NumberCell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tridas.io.AbstractDendroFileReader;
+import org.tridas.io.DendroFileFilter;
+import org.tridas.io.I18n;
+import org.tridas.io.defaults.IMetadataFieldSet;
+import org.tridas.io.exceptions.ConversionWarning;
+import org.tridas.io.exceptions.IncorrectDefaultFieldsException;
+import org.tridas.io.exceptions.InvalidDendroFileException;
+import org.tridas.io.exceptions.ConversionWarning.WarningType;
+import org.tridas.io.exceptions.InvalidDendroFileException.PointerType;
+import org.tridas.io.util.SafeIntYear;
+import org.tridas.schema.DatingSuffix;
+import org.tridas.schema.NormalTridasUnit;
+import org.tridas.schema.NormalTridasVariable;
+import org.tridas.schema.TridasElement;
+import org.tridas.schema.TridasInterpretation;
+import org.tridas.schema.TridasMeasurementSeries;
+import org.tridas.schema.TridasObject;
+import org.tridas.schema.TridasProject;
+import org.tridas.schema.TridasRadius;
+import org.tridas.schema.TridasSample;
+import org.tridas.schema.TridasTridas;
+import org.tridas.schema.TridasUnit;
+import org.tridas.schema.TridasValue;
+import org.tridas.schema.TridasValues;
+import org.tridas.schema.TridasVariable;
+
+public class ODFMatrixReader extends AbstractDendroFileReader {
+
+	private static final Logger log = LoggerFactory.getLogger(ODFMatrixReader.class);
+	private ODFMatrixToTridasDefaults defaults;
+	private Sheet sheet;
+	private Cell[] yearCol;
+	private ArrayList<ExcelDendroSeries> series = new ArrayList<ExcelDendroSeries>();
+	
+	public ODFMatrixReader()
+	{
+		super(ODFMatrixToTridasDefaults.class);
+	}
+	
+	@Override
+	public DendroFileFilter getDendroFileFilter() {
+		String[] exts = new String[] {"xls"};
+		
+		return new DendroFileFilter(exts, getShortName());
+
+	}
+
+	@Override
+	public String getDescription() {
+		return I18n.getText("odfmatrix.about.description");
+	}
+
+	@Override
+	public String[] getFileExtensions() {
+		return new String[]{"xls"};
+	}
+
+	@Override
+	public String getFullName() {
+		return I18n.getText("odfmatrix.about.fullName");
+	}
+
+	@Override
+	public String getShortName() {
+		return I18n.getText("odfmatrix.about.shortName");
+	}
+
+	// *******************************
+	// NOT SUPPORTED - BINARY FORMAT
+	// *******************************
+	
+	@Override
+	protected void parseFile(String[] argFileString, IMetadataFieldSet argDefaultFields) {
+		throw new UnsupportedOperationException(I18n.getText("general.binaryNotText"));
+	}
+	
+	@Override
+	public void loadFile(String[] argFileStrings) throws InvalidDendroFileException {
+		throw new UnsupportedOperationException(I18n.getText("general.binaryNotText"));
+	}
+		
+	
+	/**
+	 * @throws IncorrectDefaultFieldsException
+	 * @throws InvalidDendroFileException
+	 * @see org.tridas.io.IDendroCollectionWriter#loadFile(java.lang.String)
+	 */
+	@Override
+	public void loadFile(String argFilename, IMetadataFieldSet argDefaultFields) throws IOException,
+			IncorrectDefaultFieldsException, InvalidDendroFileException {
+		
+		log.debug("loading file from: " + argFilename);
+		defaults = (ODFMatrixToTridasDefaults) argDefaultFields;
+		File file = new File(argFilename);
+		
+		try {
+			Workbook wb = Workbook.getWorkbook(file);
+			parseFile(wb);
+		} catch (BiffException e) {
+			throw new InvalidDendroFileException(e.getMessage());
+		}
+
+	}
+	
+	@Override
+	public void loadFile(String argPath, String argFilename, IMetadataFieldSet argDefaultFields) throws IOException,
+			IncorrectDefaultFieldsException, InvalidDendroFileException {
+		
+		log.debug("loading file from: " + argPath + File.separatorChar + argFilename);
+		defaults = (ODFMatrixToTridasDefaults) argDefaultFields;
+		File file = new File(argPath + File.separatorChar + argFilename);
+		
+		try {
+			Workbook wb = Workbook.getWorkbook(file);
+			parseFile(wb);
+		} catch (BiffException e) {
+			throw new InvalidDendroFileException(e.getMessage());
+		}
+
+	}
+		
+	/**
+	 * Check this is a valid Excel file
+	 * 
+	 * @param argFileBytes
+	 * @throws InvalidDendroFileException
+	 */
+	protected void parseFile(Workbook wb) throws InvalidDendroFileException{
+	
+		if(wb==null) throw new InvalidDendroFileException(I18n.getText("excelmatrix.workbookError"));
+		
+		if(wb.getSheets().length>1)
+		{
+			this.addWarning(new ConversionWarning(WarningType.IGNORED, 
+					I18n.getText("excelmatrix.ignoringWorksheetsExcept",
+							wb.getSheet(0).getName())));
+		}
+		
+		sheet = wb.getSheet(0);
+
+		// Check year column is valid
+		Cell[] yearCol = sheet.getColumn(0);
+		Integer lastval = null;
+		Integer thisval = null;
+		for (int i=1; i < yearCol.length; i++)
+		{
+			// Check cell is an integer
+			try{
+				thisval = Integer.parseInt(sheet.getCell(0, i).getContents());
+				
+				if(thisval.equals(0))
+				{
+					throw new InvalidDendroFileException(
+							I18n.getText("excelmatrix.yearsNotGregorian"), 
+							"A"+String.valueOf(i), PointerType.CELL);
+				}
+				
+			} catch (NumberFormatException e)
+			{
+				throw new InvalidDendroFileException(
+						I18n.getText("excelmatrix.yearNumberExpected"), 
+						"A"+String.valueOf(i), PointerType.CELL);
+			}
+			
+
+			
+			if (lastval==null) 
+			{
+				// First year 
+				lastval = thisval;
+				continue;
+			}
+			
+			SafeIntYear previousYear = new SafeIntYear(lastval);
+			SafeIntYear thisYear = new SafeIntYear(thisval);
+			
+			if(previousYear.add(1).equals(thisYear))
+			{
+				// Next year in sequence - so ok
+				lastval = thisval;
+				continue;
+			}
+			else
+			{
+				throw new InvalidDendroFileException(
+						I18n.getText("excelmatrix.invalidYearSequence"), 
+						"A"+String.valueOf(i+1), PointerType.CELL);
+			}
+		}
+		
+		// Loop through data columns
+		for(int i=1; i < sheet.getColumns(); i++)
+		{
+			Cell[] datacol = sheet.getColumn(i);
+			ExcelDendroSeries edc = new ExcelDendroSeries();
+			
+			// Throw a wobbly if header is empty
+			if(datacol[0].getContents().equals("")) 
+			{
+				throw new InvalidDendroFileException(
+						I18n.getText("excelmatrix.emptyHeader"), 
+						getColRef(i)+"1", PointerType.CELL);
+			}
+			
+			// Warn if there is more data than years
+			if(datacol.length>yearCol.length)
+			{
+				this.addWarning(new ConversionWarning(WarningType.IGNORED, 
+						I18n.getText("excelmatrix.moreDataThanYears",
+						getColRef(i))));
+			}
+					
+			// Compile a list of the data values
+			ArrayList<Double> dataVals = new ArrayList<Double>();
+			Boolean atStartOfData =false;
+			for(int j=1; j<datacol.length; j++)
+			{
+				if(atStartOfData== false && datacol[j].getContents().equals(""))
+				{
+					continue;
+				}
+				else if (atStartOfData == false)
+				{
+					atStartOfData = true;
+					edc.startYear = this.getYearForRow(j);
+				}
+				else if(atStartOfData== true && datacol[j].getContents().equals(""))
+				{
+					break;
+				}
+				
+				
+				try{ 
+					if(datacol[j].getType() != CellType.NUMBER)
+					{
+						throw new InvalidDendroFileException(
+								I18n.getText("excelmatrix.invalidDataValue"), 
+								getColRef(i)+String.valueOf(j+1), 
+								PointerType.CELL);
+					}
+					
+					NumberCell nc = (NumberCell) datacol[j];
+					dataVals.add(nc.getValue());
+					
+					if(nc.getValue()>10d)
+					{
+						this.addWarning(new ConversionWarning(WarningType.ASSUMPTION, 
+								I18n.getText("excelmatrix.largeDataValue")));
+					}
+					
+				} catch (NumberFormatException e)
+				{
+					throw new InvalidDendroFileException(
+							I18n.getText("excelmatrix.invalidDataValue"), 
+							getColRef(i)+String.valueOf(j+1), 
+							PointerType.CELL);
+				}
+			}
+			
+			edc.label = datacol[0].getContents();
+			edc.defaults = defaults;
+			edc.dataVals = dataVals;
+			series.add(edc);
+			
+			
+		}
+		
+
+
+	}
+	
+	private SafeIntYear getYearForRow(int row)
+	{
+		try{
+			Cell cell = sheet.getCell(0, row);
+			return new SafeIntYear(cell.getContents());
+		} catch (NumberFormatException e)
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * Get the Excel column reference for a column number 
+	 * 
+	 * @param col <= 676
+	 * @return
+	 */
+	private String getColRef(int col)
+	{
+		String colcodes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				
+		if(col<0) return null;
+		
+		if(col > 676)
+		{
+			System.out.println("Error.  getColRef called with number out of range");
+			return "??";
+		}
+		else if (col < 26)
+		{
+			return String.valueOf(colcodes.charAt(col));
+		}
+		else
+		{
+			int quotient = col / 26;
+			int remainder = col % 26;
+			return String.valueOf(colcodes.charAt(quotient-1)) + String.valueOf(colcodes.charAt(remainder));
+		}		
+	}
+	
+	@Override
+	protected void resetReader() {
+		sheet = null;
+		yearCol = null;
+		defaults = null;
+
+	}
+	
+	@Override
+	public int getCurrentLineNumber() {
+		return 0;
+	}
+
+	@Override
+	public IMetadataFieldSet getDefaults() {
+		return defaults;
+	}
+
+	private TridasProject getProject() {
+		TridasProject project = defaults.getProjectWithDefaults();
+		
+		for (ExcelDendroSeries eds : series)
+		{
+			TridasObject o = eds.defaults.getObjectWithDefaults(true);
+			TridasMeasurementSeries ms = o.getElements().get(0).getSamples().get(0).getRadiuses().get(0).getMeasurementSeries().get(0);
+			
+			ms.setTitle(eds.label);
+			
+			TridasInterpretation interp = new TridasInterpretation();
+			interp.setFirstYear(eds.startYear.toTridasYear(DatingSuffix.AD));
+			ms.setInterpretation(interp);
+			
+			ArrayList<TridasValue> valuesList = new ArrayList<TridasValue>();
+			for(Double dbl : eds.dataVals)
+			{
+				TridasValue val = new TridasValue();
+				val.setValue(dbl.toString());
+				valuesList.add(val);
+			}
+			
+			TridasValues valuesGroup = new TridasValues();
+			TridasVariable variable = new TridasVariable();
+			variable.setNormalTridas(NormalTridasVariable.RING_WIDTH);
+			TridasUnit units = new TridasUnit();
+			units.setNormalTridas(NormalTridasUnit.MILLIMETRES);
+			
+			valuesGroup.setVariable(variable);
+			valuesGroup.setUnit(units);
+			valuesGroup.setValues(valuesList);
+
+			ms.getValues().add(valuesGroup);
+			
+			project.getObjects().add(o);
+		}
+		
+		
+		
+		return project;
+	}
+
+	private static class ExcelDendroSeries {
+		public ODFMatrixToTridasDefaults defaults;
+		public SafeIntYear startYear;
+		public String label;
+		public ArrayList<Double> dataVals = new ArrayList<Double>();
+		
+	}
+	
+	/**
+	 * @see org.tridas.io.AbstractDendroFileReader#getProjects()
+	 */
+	@Override
+	public TridasProject[] getProjects() {
+		TridasProject projects[] = new TridasProject[1];
+		projects[0] = this.getProject();
+		return projects;
+	}
+
+	/**
+	 * @see org.tridas.io.AbstractDendroFileReader#getTridasContainer()
+	 */
+	public TridasTridas getTridasContainer() {
+		TridasTridas container = new TridasTridas();
+		List<TridasProject> list = Arrays.asList(getProjects());
+		container.setProjects(list);
+		return container;
+	}
+
+}
+
