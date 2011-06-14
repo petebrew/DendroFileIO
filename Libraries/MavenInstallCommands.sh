35cb46506225de298d53a2c6caf6f8574a45c34f
Peter Brewer <p.brewer@ltrr.arizona.edu>
2011-06-14 17:09:18 +0000

    Adding jama jar

--- Libraries/MavenInstallCommands.sh
+++ Libraries/MavenInstallCommands.sh
@@ -2,4 +2,5 @@
 # placed in the org.tridas.maven repository but are included here in case this repository becomes
 # unavailable.  The jars can be installed with the following commands:
 
-mvn install:install-file -DgroupId=com.jhlabs    -DartifactId=jmapprojlib     -Dversion=1.2.0-SNAPSHOT    -Dpackaging=jar -Dfile=jmapprojlib-1.2.0-SNAPSHOT.jar
+mvn install:install-file -DgroupId=com.jhlabs    -DartifactId=jmapprojlib   -Dversion=1.2.0    -Dpackaging=jar -Dfile=jmapprojlib-1.2.0-SNAPSHOT.jar
+mvn install:install-file -DgroupId=gov.nist.math -DartifactId=jama          -Dversion=1.0.2    -Dpackaging=jar -Dfile=jama-1.0.2.jar
