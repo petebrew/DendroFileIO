9215c17eae5d42fe658ddcbc962391b05ede8586
sbr00pwb <sbr00pwb@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2011-06-15 21:34:59 +0000

    Moving Poms to point at new maven.tridas.org repository

--- DendroLibrary/DendroFileIO/Libraries/MavenInstallCommands.sh
+++ DendroLibrary/DendroFileIO/Libraries/MavenInstallCommands.sh
@@ -1,5 +1,5 @@
 # The Jars within this folder are those not found in standard Maven repositories.  They have been
-# placed in the org.tridas.maven repository but are included here in case this repository becomes
+# placed in the maven.tridas.org repository but are included here in case this repository becomes
 # unavailable.  The jars can be installed with the following commands:
 
 mvn install:install-file -DgroupId=com.jhlabs    -DartifactId=jmapprojlib   -Dversion=1.2.0    -Dpackaging=jar -Dfile=jmapprojlib-1.2.0-SNAPSHOT.jar
